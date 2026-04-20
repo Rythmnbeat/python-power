0b956bc39cb7a8404812a8ee6246ae8d067593a5
Rythmnbeat <138241677+Rythmnbeat@users.noreply.github.com>
2026-04-20 04:12:56 +0000

    project organizer finishing

--- TermProject.py
+++ TermProject.py
@@ -22,15 +22,16 @@ def filter_file(filename):
     else:
         return "other"
 
-def movefile(srcPath, dest_folder):
+def move_file(srcPath, dest_folder):
     try:
         os.makedirs(dest_folder, exist_ok=True) #make path incase not already present
         shutil.move(srcPath, dest_folder)
     except Exception as e:
-        print("Error moving file: {e}")
+        print(f"Error moving file: {e}")
+        raise e
 
 
-def logging(logged):
+def log_Action(logged):
     try:
         with open("log.txt", "a") as log:
             log.write(logged + "\n")
@@ -40,7 +41,8 @@ def logging(logged):
 def main():
     
     folder_path = input("Enter folder path: ")
-
+    folder_path = folder_path.strip().replace("\\", "/")
+    print(folder_path)
     if os.path.isdir(folder_path):
         print("Valid path!")
     else: 
@@ -50,26 +52,27 @@ def main():
         files = directory_scan(folder_path)
     except Exception as e:
         print(f"Error scanning directory: {e}")
+        return 
     
     for file in files:
         full_path = os.path.join(folder_path, file)
 
-        try:
-            if os.path.isfile(full_path):
+        if os.path.isfile(full_path):
+            try:
                 categorize = filter_file(file)
                 destination = os.path.join(folder_path, categorize)
-                movefile(full_path, destination)
-                logging(f"{file} moved to {categorize}")
+                move_file(full_path, destination)
+                log_Action(f"{file} moved to {categorize}")
 
-        except PermissionError:
-            print("Permission Denied")
-            logging(f"failed to move {file}: Permission Denied")
-        except FileNotFoundError:
-            print(f"File: {file} cannot be found anymore")
-            logging(f"failed to move {file}: File not found")
-        except Exception as e:
-            print(f"Unexpected error with {file}: {e}")
-            logging(f"failed to move: {file} because {e}")
+            except PermissionError:
+                print("Permission Denied")
+                log_Action(f"failed to move {file}: Permission Denied")
+            except FileNotFoundError:
+                print(f"File: {file} cannot be found anymore")
+                log_Action(f"failed to move {file}: File not found")
+            except Exception as e:
+                print(f"Unexpected error with {file}: {e}")
+                log_Action(f"failed to move: {file} because {e}")
 
     print("Organizing completed")
 
