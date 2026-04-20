74a615de4a005285f2075633b2d4032b1b6b0c11
Rythmnbeat <138241677+Rythmnbeat@users.noreply.github.com>
2026-04-20 02:57:19 +0000

    error handling

--- TermProject.py
+++ TermProject.py
@@ -4,8 +4,12 @@ import shutil
 
 
 def directory_scan(folder_path):
-    print(f"scanning {folder_path}")
-    return os.listdr(folder_path)
+    try:
+        print(f"scanning {folder_path}")
+        return os.listdir(folder_path)
+    except Exception as e:
+        raise e
+
 
 def filter_file(filename):
     extention = filename.split(".")[-1].lower() #pull file extention part/making them lower case
@@ -13,18 +17,25 @@ def filter_file(filename):
         return "images"
     elif extention in ["pdf", "docx", "txt"]:
         return "documents"
-    elif extention in ["mp3", "wav", "AIFF", "AAC"]:
+    elif extention in ["mp3", "wav", "aiff", "aac"]:
         return "audio"
     else:
         return "other"
 
 def movefile(srcPath, dest_folder):
-    os.makedirs(dest_folder, exist_ok=True) #make path incase not already present
-    shutil.move(srcPath, dest_folder)
+    try:
+        os.makedirs(dest_folder, exist_ok=True) #make path incase not already present
+        shutil.move(srcPath, dest_folder)
+    except Exception as e:
+        print("Error moving file: {e}")
 
-def logging(logger)
-    with open("log.txt", "a") as log:
-        log.write(logger, + "\n")
+
+def logging(logger):
+    try:
+        with open("log.txt", "a") as log:
+            log.write(logger + "\n")
+    except Exception as e:
+        print(f"Failed logging: {e}")
 
 def main():
     
@@ -34,17 +45,31 @@ def main():
         print("Valid path!")
     else: 
         print("That folder does not exist.")
+        return
+    try:
+        files = directory_scan(folder_path)
+    except Exception as e:
+        print(f"Error scanning directory: {e}")
     
-    files = directory_scan(folder_path)
-
     for file in files:
         full_path = os.path.join(folder_path, file)
 
-        if os.path.isfile(full_path):
-            categorize = filter_file(file)
-            destination = os.path.join(folder_path, categorize)
-            movefile(full_path, destination)
-            logging(f"{file} moved to {categorize}")
+        try:
+            if os.path.isfile(full_path):
+                categorize = filter_file(file)
+                destination = os.path.join(folder_path, categorize)
+                movefile(full_path, destination)
+                logging(f"{file} moved to {categorize}")
+
+        except PermissionError:
+            print("Permission Denied")
+            logging(f"failed to move {file}: Permission Denied")
+        except FileNotFoundError:
+            print(f"File: {file} cannot be found anymore")
+            logging(f"failed to move {file}: File not found")
+        except Exception as e:
+            print(f"Unexpected error with {file}: {e}")
+            logging(f"failed to move: {file} because {e}")
 
     print("Organizing complete")
 
